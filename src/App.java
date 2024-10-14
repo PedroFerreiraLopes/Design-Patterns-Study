f65859cfb3e349edcf0ecf965569660cba290bce
Anna Victoria Feliciana <anna.feliciana@upe.br>
2024-10-14 18:54:31 -0300

    teste main

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import br.entrega.EntregaNormal;
+import br.entrega.Pedido;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //testing Pedido
+        Pedido pedido1 = new Pedido("1", new EntregaNormal(), "Rua A", "Rua B", "João", "Maria");
+        pedido1.infoPedido();
     }
 }
